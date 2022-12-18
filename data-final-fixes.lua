65bee66eaa61dfce7a4c6c32d16e9664dfbecc26
robot256 <robot256@GMAIL.COM>
2022-12-17 22:56:48 -0500

    Fix crash with un-minable gates
    Closes #2.
    Fixes compatibility with Laser Fence mod.

--- data-final-fixes.lua
+++ data-final-fixes.lua
@@ -1,7 +1,11 @@
 
 -- Make gates take longer to mine, so you can mine a belt off them
 for name,gate in pairs(data.raw.gate) do
-  if gate.minable.mining_time < 0.75 then
-    gate.minable.mining_time = 0.75
+  if gate.minable then
+    if gate.minable.mining_time < 0.75 then
+      gate.minable.mining_time = 0.75
+    end
+  else
+    log("Note: gate entity '"..name.."' is not minable!")
   end
 end
